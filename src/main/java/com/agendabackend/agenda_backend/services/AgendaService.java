23bcfc94f699cce276b4c40a003700d5f977a3e2
carlosjc31 <carlosjc31@hotmail.com>
2024-11-30 12:26:21 -0300

    7º conf. AgendaService

--- src/main/java/com/agendabackend/agenda_backend/services/AgendaService.java
+++ src/main/java/com/agendabackend/agenda_backend/services/AgendaService.java
@@ -1,5 +1,22 @@
 package com.agendabackend.agenda_backend.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.agendabackend.agenda_backend.dtos.AgendaResponse;
+import com.agendabackend.agenda_backend.mappers.AgendaMapper;
+import com.agendabackend.agenda_backend.repositories.AgendaRepository;
+
+@Service
 public class AgendaService {
     
+    @Autowired
+    private AgendaRepository repository;
+
+    public List<AgendaResponse> getAllAgenda(){
+        return repository.findAll().stream().map(a -> AgendaMapper.toDTO(a)).collect(Collectors.toList());
+    }
 }
